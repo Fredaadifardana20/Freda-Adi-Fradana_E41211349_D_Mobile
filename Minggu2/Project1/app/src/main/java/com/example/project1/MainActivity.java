6b5d38de3caef51156d3cd02ea812883ba901674
fredaadifardana <fredadifar@gmail.com>
2023-02-22 06:39:08 +0700

    update

--- Minggu2/Project1/app/src/main/java/com/example/project1/MainActivity.java
+++ Minggu2/Project1/app/src/main/java/com/example/project1/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.);
+        setContentView(R.layout.mainmenu);
     }
     public void menu(View view){
         Intent intent = new Intent(MainActivity.this,activity_linear.class);
